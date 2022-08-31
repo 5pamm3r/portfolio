cef77a34d958e7aaec8dacc4a9f06b8c2c247672
5pamm3r <cmassarino.dev@gmail.com>
2022-08-31 15:55:30 -0300

    add contact and project containers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import { contactContainer, fragment } from './contact.js'
-const contact = document.querySelector('#contact')
+import { contactContainer, fragment } from "./contact.js";
+import { projects, fragmentProject } from "./projects.js";
 
+const getContact = (() => {
+  const contact = document.querySelector("#contact");
+  contactContainer(
+    "https://www.linkedin.com/in/carlos-massarino-9970b1234/",
+    "../public/assets/icons/linkedin-black.svg"
+  );
+  contactContainer(
+    "https://twitter.com/C_Massarino",
+    "../public/assets/icons/twitter-black.svg"
+  );
+  contactContainer(
+    "https://github.com/5pamm3r",
+    "../public/assets/icons/github.png"
+  );
+  contact.append(fragment);
+})();
 
-contactContainer('https://www.linkedin.com/in/carlos-massarino-9970b1234/', '../public/assets/icons/linkedin.png');
-contactContainer('https://twitter.com/C_Massarino', '../public/assets/icons/twitter.png');
-contactContainer('https://github.com/5pamm3r', '../public/assets/icons/github.png');
-contact.append(fragment)
+const getProject = (() => {
+  const project = document.querySelector('#projects');
+  project.append(fragmentProject)
+})();

e6915201868da7ab0dbaff80356ec88b97b888f2
5pamm3r <cmassarino.dev@gmail.com>
2022-11-12 14:19:36 -0300

    Add more items

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,12 @@
 const fragmentProject = document.createDocumentFragment()
 
-const projects = (title, subtitle, description, image, video) => {
+const projects = (title, subtitle, description1, description2, description3, image, video) => {
   const projectsContainer = document.querySelector('#templateProjects').content;
-  projectsContainer.querySelector('#projectTitle').value = title;
-  projectsContainer.querySelector('#projectSubtitle').value = subtitle;
-  projectsContainer.querySelector('#projectDescription').value = description;
+  projectsContainer.querySelector('#projectTitle').textContent = title;
+  projectsContainer.querySelector('#projectSubtitle').textContent = subtitle;
+  projectsContainer.querySelector('#projectDescription1').textContent = description1;
+  projectsContainer.querySelector('#projectDescription2').textContent = description2;
+  projectsContainer.querySelector('#projectDescription3').textContent = description3;
   projectsContainer.querySelector('#projectImg').src = image;
   projectsContainer.querySelector('#projectVideo').src = video;
 

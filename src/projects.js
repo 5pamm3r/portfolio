6f65a7a8007fd1ec1dae9525445f2bd95ddcd077
5pamm3r <cmassarino.dev@gmail.com>
2023-02-02 11:35:36 -0300

    Add projects links

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,13 @@
 const fragmentProject = document.createDocumentFragment()
 
-const projects = (title, subtitle, description1, description2, description3, image, video) => {
+const projects = (title, subtitle, description, image, video, link) => {
   const projectsContainer = document.querySelector('#templateProjects').content;
   projectsContainer.querySelector('#projectTitle').textContent = title;
   projectsContainer.querySelector('#projectSubtitle').textContent = subtitle;
-  projectsContainer.querySelector('#projectDescription1').textContent = description1;
-  projectsContainer.querySelector('#projectDescription2').textContent = description2;
-  projectsContainer.querySelector('#projectDescription3').textContent = description3;
+  projectsContainer.querySelector('#projectDescription').textContent = description;
   projectsContainer.querySelector('#projectImg').src = image;
   projectsContainer.querySelector('#projectVideo').src = video;
+  projectsContainer.querySelector('#projectLink').href = link;
 
   const clone = document.importNode(projectsContainer, true);
   fragmentProject.append(clone);

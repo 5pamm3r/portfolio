deca3dc5f8768aa32153de6976e26ca97422763a
5pamm3r <cmassarino.dev@gmail.com>
2023-02-22 19:37:30 -0300

    Add new skills

---
+++ src/skills.js
@@ -0,0 +1,37 @@
+export const SKILLS = [
+  { 
+    name: 'HTML',
+    icon: '../public/assets/icons/html.png',
+  },
+  { name: 'CSS',
+    icon: '../public/assets/icons/css3.png',
+  },
+  {
+    name: 'JS',
+    icon: '../public/assets/icons/javascript.png',
+  },
+  {
+    name: 'React',
+    icon: '../public/assets/icons/react.png',
+  },
+  {
+   name: 'SASS',
+   icon: '../public/assets/icons/sass.png',
+  },
+  {
+    name: 'TypeScript',
+    icon: '../public/assets/icons/typescript.png'
+  },
+  {
+    name: 'MySQL',
+    icon: '../public/assets/icons/mysql.png'
+  },
+  {
+    name: 'NodeJs',
+    icon: '../public/assets/icons/node.png',
+  },
+  {
+    name: 'PostgreSQL',
+    icon: '../public/assets/icons/postgres.png',
+  },
+]

f57db3d3cde3fbc614c262813aeba8458427f2f0
5pamm3r <cmassarino.dev@gmail.com>
2023-02-22 19:38:18 -0300

    Change class for function

--- src/skillsList.js
+++ src/skillsList.js
@@ -1,37 +1,13 @@
-import {iconsSkill} from './iconsSkill.js'
-class SkillsList extends HTMLElement {
-  constructor() {
-    super();
-    //Referencia a la raíz del elemento. 
-    const shadow = this.attachShadow({mode: 'open'})
-    const style = document.createElement('style')
-    style.textContent = `
-    .skills-area__images {
-        padding: 0;
-        display: flex;
-        gap: 20px;
-      }
-    .skills-area__images img {
-      width: 50px;
-    }
-    `
-    this.ulHeader = document.createElement('ul')
-    this.ulHeader.setAttribute('class', 'skills-area__images')
-    let arr = []
-    iconsSkill.forEach(e=>{
-      const el = document.createElement('img')
-      el.src = e.icon
-      el.title = e.name
-      arr.push(el)
-    })
-    shadow.appendChild(style)
-    shadow.appendChild(this.ulHeader)
-    this.ulHeader.append(...arr)
-  }
-  //Obtiene los atributos.
-  connectedCallback(){
-    
-  }
-}
+function skillsList(name, url) {
+  const $container = document.createElement("figure");
+  $container.classList.add("skill__container");
+  const $text = document.createElement("figcaption");
+  $text.textContent = name;
+  const $image = document.createElement("img");
+  $image.src = url;
+  $image.alt = name;
 
-customElements.define('skills-list', SkillsList)
+  $container.append($image, $text);
+  document.getElementById("skillsList").append($container);
+}
+export { skillsList };

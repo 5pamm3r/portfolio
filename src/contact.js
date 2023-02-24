6ed5d358097d8ec20ad642a8f45766aa578146dd
5pamm3r <cmassarino.dev@gmail.com>
2023-02-24 00:00:44 -0300

    Add id

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,10 @@
 const fragment = document.createDocumentFragment()
 
-const contactContainer = (link, image) => {
+const contactContainer = (id, link, image) => {
   const contactContainer = document.querySelector('#templateContact').content;
-  contactContainer.querySelector('#contactLink').href = link;
-  contactContainer.querySelector('#contactImg').src = image;
+  contactContainer.querySelector('.imageContact__container').id = id;
+  contactContainer.querySelector('.contactLink').href = link;
+  contactContainer.querySelector('.contactImg').src = image;
   const clone = document.importNode(contactContainer, true)
   fragment.append(clone)
 

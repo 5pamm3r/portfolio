096f5a1b614ddcacaa70477c35f4781ca93a1b2e
5pamm3r <cmassarino.dev@gmail.com>
2023-02-27 19:32:41 -0300

    Add list container

--- src/skillsList.js
+++ src/skillsList.js
@@ -1,4 +1,5 @@
 function skillsList(name, url) {
+  const $list = document.createElement('li');
   const $container = document.createElement("figure");
   $container.classList.add("skill__container");
   const $text = document.createElement("figcaption");
@@ -8,6 +9,7 @@ function skillsList(name, url) {
   $image.alt = name;
 
   $container.append($image, $text);
-  document.getElementById("skillsList").append($container);
+  $list.append($container)
+  document.getElementById("skillsList").append($list);
 }
 export { skillsList };

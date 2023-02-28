0bf2863d8f96d89844f63f7341b16d35827bff8f
5pamm3r <cmassarino.dev@gmail.com>
2023-02-28 19:54:08 -0300

    Add video url

--- src/index.js
+++ src/index.js
@@ -31,13 +31,19 @@ const getProject = (() => {
     'Expense tracker',
     'React, UX/UI Design',
     'Do you feel that money flies? To optimize expenses, it is important to know what is spent on. There is only one way to do it and that is to keep track of expenses.',
-    'https://5pamm3r.github.io/portfolio/public/assets/images/cryptoWallet.png'
+    'https://5pamm3r.github.io/portfolio/public/assets/images/cryptoWallet.png',
+    'https://5pamm3r.github.io/portfolio/public/assets/videos/expenseTracker-video.mp4',
+    'https://github.com/5pamm3r/expense-tracker'
+
   )
   projects(
     'Umbrella',
     'JavaScript, UX/UI Design',
     'Are you worried about the weather? In this app you will find basic information such as the current weather situation, as well as the forecast for days or hours.',
-    'https://5pamm3r.github.io/portfolio/public/assets/images/umbrella.png'
+    'https://5pamm3r.github.io/portfolio/public/assets/images/umbrella.png',
+    'https://5pamm3r.github.io/portfolio/public/assets/videos/umbrella-video.mp4',
+    'https://github.com/5pamm3r/umbrella'
+
     
   )
   projects(

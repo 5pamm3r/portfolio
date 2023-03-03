c905c2bcc2ab6beeff417d55ba5ab234a901fd92
5pamm3r <cmassarino.dev@gmail.com>
2023-03-03 19:28:31 -0300

    Add link to video

--- src/projects.js
+++ src/projects.js
@@ -1,17 +1,16 @@
-const fragmentProject = document.createDocumentFragment()
+const fragmentProject = document.createDocumentFragment();
 
 const projects = (title, subtitle, description, image, video, link) => {
-  const projectsContainer = document.querySelector('#templateProjects').content;
-  projectsContainer.querySelector('#projectTitle').textContent = title;
-  projectsContainer.querySelector('#projectSubtitle').textContent = subtitle;
-  projectsContainer.querySelector('#projectDescription').textContent = description;
-  projectsContainer.querySelector('#projectImg').src = image;
-  projectsContainer.querySelector('#projectVideo').src = video;
-  projectsContainer.querySelector('#projectLink').href = link;
+  const projectsContainer = document.getElementById("templateProjects").content;
+  projectsContainer.getElementById("projectTitle").textContent = title;
+  projectsContainer.getElementById("projectSubtitle").textContent = subtitle;
+  projectsContainer.getElementById("projectDescription").textContent = description;
+  projectsContainer.getElementById("projectImg").src = image;
+  projectsContainer.getElementById("projectVideo").src = video;
+  projectsContainer.getElementById("projectLink").href = link;
+  projectsContainer.getElementById("videoLink").href = link;
 
   const clone = document.importNode(projectsContainer, true);
   fragmentProject.append(clone);
-
-}
-export { projects, fragmentProject}
-
+};
+export { projects, fragmentProject };

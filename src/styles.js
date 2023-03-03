87e62ce4152d0d18da9fa086352054225c300ca9
5pamm3r <cmassarino.dev@gmail.com>
2023-03-03 00:39:01 -0300

    Add accordion

--- src/styles.js
+++ src/styles.js
@@ -1,70 +1,82 @@
 function styles() {
-  document.getElementById("faq-item1").addEventListener("click", (e) => {
+  document.getElementById("faq-accordion1").addEventListener("click", (e) => {
     const container = document.getElementById("faqHidden-item1");
-    const boxHeight = document.getElementById('hidden1').clientHeight;
     if (container.classList.contains("faq-hidden")) {
       container.classList.remove("faq-hidden");
       container.classList.add("faq-visible");
       document.getElementById("faqIcon1").classList.add("active");
-      document.getElementById("hidden1").classList.add("active");
-      document.getElementById("hidden1").classList.remove("hidden");
-      document.getElementById('faqHidden-item1').style.height = `${boxHeight + 40}px`
+      document.getElementById("sect1").classList.add("active");
+      document.getElementById("sect1").removeAttribute("hidden");
+      document.getElementById("faqHidden-item1").style.height = '200px'
+      setTimeout(() => {
+        document.getElementById('sect1').style.opacity = '1';
+      }, 300);
     } else {
       container.classList.remove("faq-visible");
       container.classList.add("faq-hidden");
       document.getElementById("faqIcon1").classList.remove("active");
-      
-      document.getElementById('faqHidden-item1').style.height = '20px'
-      document.getElementById("hidden1").classList.add("hidden");
-      document.getElementById("hidden1").classList.remove("active");
+
+      document.getElementById("faqHidden-item1").style.height = "20px";
+      document.getElementById("sect1").classList.remove("active");
+      setTimeout(() => {
+        document.getElementById("sect1").setAttribute("hidden", "");
+        document.getElementById('sect1').style.opacity = '0';
+      }, 300);
+
     }
   });
-  document.getElementById("faq-item2").addEventListener("click", (e) => {
+  document.getElementById("faq-accordion2").addEventListener("click", (e) => {
     const container = document.getElementById("faqHidden-item2");
-    const boxHeight = document.getElementById('hidden2').clientHeight;
     if (container.classList.contains("faq-hidden")) {
       container.classList.remove("faq-hidden");
       container.classList.add("faq-visible");
       document.getElementById("faqIcon2").classList.add("active");
-
-      document.getElementById("hidden2").classList.add("active");
-      document.getElementById("hidden2").classList.remove("hidden");
-      document.getElementById('faqHidden-item2').style.height = `${boxHeight + 40}px`
+      document.getElementById("sect2").classList.add("active");
+      document.getElementById("sect2").removeAttribute("hidden");
+      document.getElementById("faqHidden-item2").style.height = '200px'
+      setTimeout(() => {
+        
+        document.getElementById('sect2').style.opacity = '1';
+      }, 300);
 
     } else {
       container.classList.remove("faq-visible");
       container.classList.add("faq-hidden");
       document.getElementById("faqIcon2").classList.remove("active");
-
-      document.getElementById("hidden2").classList.add("hidden");
-      document.getElementById("hidden2").classList.remove("active");
-      document.getElementById('faqHidden-item2').style.height = `0px`
-
+      
+      document.getElementById("faqHidden-item2").style.height = '20px';
+      document.getElementById("sect2").classList.remove("active");
+      setTimeout(() => {
+        document.getElementById("sect2").setAttribute("hidden", "");
+        document.getElementById('sect2').style.opacity = '0';
+        
+      }, 300);
     }
   });
-  document.getElementById("faq-item3").addEventListener("click", (e) => {
+  document.getElementById("faq-accordion3").addEventListener("click", (e) => {
     const container = document.getElementById("faqHidden-item3");
-    const boxHeight = document.getElementById('hidden3').clientHeight;
-
     if (container.classList.contains("faq-hidden")) {
       container.classList.remove("faq-hidden");
       container.classList.add("faq-visible");
       document.getElementById("faqIcon3").classList.add("active");
-
-      document.getElementById("hidden3").classList.add("active");
-      document.getElementById("hidden3").classList.remove("hidden");
-      document.getElementById('faqHidden-item3').style.height = `${boxHeight + 40}px`
-
+      document.getElementById("sect3").classList.add("active");
+      document.getElementById("sect3").removeAttribute("hidden");
+      document.getElementById("faqHidden-item3").style.height = '200px'
+      setTimeout(() => {
+        document.getElementById('sect3').style.opacity = '1';
+      }, 300);
     } else {
       container.classList.remove("faq-visible");
       container.classList.add("faq-hidden");
       document.getElementById("faqIcon3").classList.remove("active");
 
-      document.getElementById("hidden3").classList.add("hidden");
-      document.getElementById("hidden3").classList.remove("active");
-      document.getElementById('faqHidden-item3').style.height = '0px'
-
+      document.getElementById("faqHidden-item3").style.height = "20px";
+      document.getElementById("sect3").classList.remove("active");
+      setTimeout(() => {
+        document.getElementById("sect3").setAttribute("hidden", "");
+        document.getElementById('sect3').style.opacity = '0';
+      }, 300);
     }
   });
 }
-export { styles }
+export { styles };

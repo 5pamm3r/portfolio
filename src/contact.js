4a1dbfc6c62d8c7b222b073c5553989c83927fa4
5pamm3r <cmassarino.dev@gmail.com>
2023-03-05 19:49:02 -0300

    Add aria-label

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,12 @@
 const fragment = document.createDocumentFragment()
 
-const contactContainer = (id, link, image) => {
+const contactContainer = (id, link, image, alt, aria) => {
   const contactContainer = document.querySelector('#templateContact').content;
   contactContainer.querySelector('.imageContact__container').id = id;
   contactContainer.querySelector('.contactLink').href = link;
+  contactContainer.querySelector('.contactLink').setAttribute('aria-label', aria);
   contactContainer.querySelector('.contactImg').src = image;
+  contactContainer.querySelector('.contactImg').alt = alt;
   const clone = document.importNode(contactContainer, true)
   fragment.append(clone)
 

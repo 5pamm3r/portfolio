9fcd28c39f7fef642f2000f687ed18b5bf60fc88
5pamm3r <cmassarino.dev@gmail.com>
2023-05-18 01:01:09 -0300

    new design

--- src/animations.js
+++ src/animations.js
@@ -1,27 +1,28 @@
 function topSlide() {
-  const about = document.getElementById('about')
-  const projects = document.querySelectorAll('.projectSlide')
-  const logo = document.getElementById('linkedInContact')
-  const logo2 = document.getElementById('twitterContact')
-  const logo3 = document.getElementById('githubContact')
-  const downArrow = document.getElementById('downArrow')
-  const profile = document.getElementById('imgProfile')
-
+  const home = document.getElementById("home");
+  const about = document.getElementById("about");
+  const projects = document.querySelectorAll(".projectSlide");
+  const logo = document.getElementById("linkedInContact");
+  const logo2 = document.getElementById("twitterContact");
+  const logo3 = document.getElementById("githubContact");
+  // const downArrow = document.getElementById('downArrow')
+  const profile = document.getElementById("imgProfile");
 
   const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry=>{
-      if(entry.isIntersecting) {
-        entry.target.classList.add('visible')
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
       }
-    })
-  })
+    });
+  });
 
-  observer.observe(about)
-  projects.forEach(item=>observer.observe(item))
-  observer.observe(logo)
-  observer.observe(logo2)
-  observer.observe(logo3)
-  observer.observe(downArrow)
-  observer.observe(profile)
+  observer.observe(home);
+  observer.observe(about);
+  projects.forEach((item) => observer.observe(item));
+  observer.observe(logo);
+  observer.observe(logo2);
+  observer.observe(logo3);
+  // observer.observe(downArrow)
+  observer.observe(profile);
 }
 export { topSlide };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { contactContainer, fragment } from "./contact.js";
 import { projects, fragmentProject } from "./projects.js";
-import { styles } from './styles.js';
-import { skillsList } from './skillsList.js';
-import { SKILLS } from "./skills.js";
+import { styles } from "./styles.js";
 import { topSlide } from "./animations.js";
 
 const getContact = (id) => {
@@ -11,74 +9,64 @@ const getContact = (id) => {
     "linkedInContact",
     "https://www.linkedin.com/in/carlos-massarino-9970b1234/",
     "https://5pamm3r.github.io/portfolio/public/assets/icons/linkedin-black.svg",
-    'LinkedIn',
-    'Go to LinkedIn'
+    "LinkedIn",
+    "Go to LinkedIn"
   );
   contactContainer(
     "twitterContact",
     "https://twitter.com/C_Massarino",
     "https://5pamm3r.github.io/portfolio/public/assets/icons/twitter-black.svg",
-    'Twitter',
-    'Go to Twitter'
+    "Twitter",
+    "Go to Twitter"
   );
   contactContainer(
     "githubContact",
     "https://github.com/5pamm3r",
     "https://5pamm3r.github.io/portfolio/public/assets/icons/github.png",
-    'GitHub',
-    'Go to GitHub'
+    "GitHub",
+    "Go to GitHub"
   );
   contact.append(fragment);
 };
 
 const getProject = (() => {
-  const project = document.querySelector('#projects');
+  const project = document.querySelector("#projects");
   projects(
-    'Expense tracker',
-    'React, UX/UI Design',
-    'Do you feel that money flies? To optimize expenses, it is important to know what is spent on. There is only one way to do it and that is to keep track of expenses.',
-    'https://5pamm3r.github.io/portfolio/public/assets/images/cryptoWallet.png',
-    'https://5pamm3r.github.io/portfolio/public/assets/videos/expenseTracker-video.mp4',
-    'https://5pamm3r.github.io/expense-tracker',
-  )
+    "Expense tracker",
+    "React, UX/UI Design",
+    "Do you feel that money flies? To optimize expenses, it is important to know what is spent on. There is only one way to do it and that is to keep track of expenses.",
+    "https://5pamm3r.github.io/portfolio/public/assets/images/cryptoWallet.png",
+    "https://5pamm3r.github.io/portfolio/public/assets/videos/expenseTracker-video.mp4",
+    "https://5pamm3r.github.io/expense-tracker"
+  );
   projects(
-    'Umbrella',
-    'JavaScript, UX/UI Design',
-    'Are you worried about the weather? In this app you will find basic information such as the current weather situation, as well as the forecast for days or hours.',
-    'https://5pamm3r.github.io/portfolio/public/assets/images/umbrella.png',
-    'https://5pamm3r.github.io/portfolio/public/assets/videos/umbrella-video.mp4',
-    'https://5pamm3r.github.io/umbrella/',
-    'id2'
-    
-  )
+    "Umbrella",
+    "JavaScript, UX/UI Design",
+    "Are you worried about the weather? In this app you will find basic information such as the current weather situation, as well as the forecast for days or hours.",
+    "https://5pamm3r.github.io/portfolio/public/assets/images/umbrella.png",
+    "https://5pamm3r.github.io/portfolio/public/assets/videos/umbrella-video.mp4",
+    "https://5pamm3r.github.io/umbrella/",
+    "id2"
+  );
   projects(
-    'Wish List',
-    'React, UX/UI Design',
-    'Organize effectively and say goodbye to stress. Plan, manage and control all your tasks in an application that will improve your efficiency at work and in your personal life.',
-    'https://5pamm3r.github.io/portfolio/public/assets/images/featherPens.png',
-    'https://5pamm3r.github.io/portfolio/public/assets/videos/video-wishlist.mp4',
-    'https://5pamm3r.github.io/wish-list',
-    'id3'
-  )
-  project.append(fragmentProject)
+    "Wish List",
+    "React, UX/UI Design",
+    "Organize effectively and say goodbye to stress. Plan, manage and control all your tasks in an application that will improve your efficiency at work and in your personal life.",
+    "https://5pamm3r.github.io/portfolio/public/assets/images/featherPens.png",
+    "https://5pamm3r.github.io/portfolio/public/assets/videos/video-wishlist.mp4",
+    "https://5pamm3r.github.io/wish-list",
+    "id3"
+  );
+  project.append(fragmentProject);
 })();
 
-const getSkills = () => {
-  SKILLS.map(e=>{
-    skillsList(e.name, e.icon)
-  })
-}
-// document.getElementById('hamburger').addEventListener('click', e=>{
-//   const status = document.getElementById('hamburger').classList.contains('is-active')
-//   if(!status) {
-//     document.getElementById('hamburger').classList.add('is-active')
-//   }else {
-//     document.getElementById('hamburger').classList.remove('is-active')
-//   }
-  
-// })
-getContact('#contact')
-getContact('#footerContact')
-getSkills()
-topSlide()
-styles()
+const hours = new Date().toLocaleTimeString("en-US", {
+  hour: "numeric",
+  hour12: true,
+  minute: "2-digit",
+});
+document.getElementById("country").textContent += ` ${hours}.`;
+
+getContact("#footerContact");
+topSlide();
+styles();

4f7ad4e0b6b4e466baaf1112d442ec29bd51137a
5pamm3r <cmassarino.dev@gmail.com>
2023-05-27 10:01:58 -0300

    remove old values

--- src/animations.js
+++ src/animations.js
@@ -1,11 +1,7 @@
 function topSlide() {
   const home = document.getElementById("home");
   const about = document.getElementById("about");
-  const projects = document.querySelectorAll(".projectSlide");
-  const logo = document.getElementById("linkedInContact");
-  const logo2 = document.getElementById("twitterContact");
-  const logo3 = document.getElementById("githubContact");
-  // const downArrow = document.getElementById('downArrow')
+  const projects = document.querySelectorAll(".project__container");
   const profile = document.getElementById("imgProfile");
 
   const observer = new IntersectionObserver((entries) => {
@@ -18,11 +14,7 @@ function topSlide() {
 
   observer.observe(home);
   observer.observe(about);
-  projects.forEach((item) => observer.observe(item));
-  observer.observe(logo);
-  observer.observe(logo2);
-  observer.observe(logo3);
-  // observer.observe(downArrow)
   observer.observe(profile);
+  projects.forEach((item) => observer.observe(item));
 }
 export { topSlide };
